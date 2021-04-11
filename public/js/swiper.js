8b2e90729ab91a8025822a29cf7af86671d5a7f0
tmdwhd0123 <>
2021-04-11 14:04:01 +0000

    banner swiper

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -32,4 +32,20 @@ var swiper = new Swiper('.swiper2', {
                 el: '.swiper-pagination',
                 clickable: true,
             },
-        });
+        }); 
+
+
+
+    var swiper = new Swiper('.swiper-banner', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
