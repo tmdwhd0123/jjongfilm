837b71fd61725b782cb4e9a7e404627233d706dc
tmdwhd0123 <>
2021-04-12 13:43:32 +0000

    banner swiper change

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -23,23 +23,13 @@ var swiper = new Swiper('.swiper1', {
   // },
 });
 
-var swiper = new Swiper('.swiper2', {
-            direction: 'vertical',
-            slidesPerView: 2,
-            spaceBetween: 30,
-            mousewheel: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        }); 
-
-
-
-    var swiper = new Swiper('.swiper-banner', {
-      slidesPerView: 1,
+var swiper = new Swiper('.swiper-banner', {
       spaceBetween: 30,
-      loop: true,
+      effect: 'fade',
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -47,14 +37,5 @@ var swiper = new Swiper('.swiper2', {
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-    });
-
-var swiper = new Swiper('.swiper-items', {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
       },
     });
